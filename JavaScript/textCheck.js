598fadf948b9813d0922f256b85b6baa96e28c8d
Brandon Ison <heroice18@gmail.com>
2020-10-02 09:44:56 -0600

    Finished the profanity filter

--- JavaScript/textCheck.js
+++ JavaScript/textCheck.js
@@ -1,31 +1,35 @@
-// extend strings with the method "contains"
-String.prototype.contains = function(str) { return this.indexOf(str) != -1; };
 
-// profanities of choice
+
+function profanityCheck(){
+
+String.prototype.includes = function(str) { return this.indexOf(str) != -1; };
+var stringData = document.getElementById("input-text").value;
+
+stringData = stringData.toLowerCase();
+
+console.log("VALUE IS: " + stringData);
+
 var profanities = new Array("ass", "cunt", "pope", "fuck", "bitch", "damn", 
 "asshole", "faggot", "motherfucker", "dick", "pussy", 
 "bastard", );
 
-var containsProfanity = function(text){
-    var returnVal = false; 
-    for (var i = 0; i < profanities.length; i++) {
-        if(text.toLowerCase().contains(profanities[i].toLowerCase())){
-            returnVal = true;
-            break;
-        }
+var doesInclude = false;
+
+doesInclude = profanities.some(word => stringData.includes(word));
+
+/*
+Here Cameron we can make it so that the AI responds or have an
+Alert that alerts the user to not use profanity. 
+*/
+if(doesInclude == true)
+    {
+        alert("Please do not write profanity into the bar.");
     }
-    return returnVal;
-}
-    
-var myText = $('#text').html();
+else
+    {
+        //alert("NO CURSES");
+        //All good
+    }
 
-if(containsProfanity(myText)){
-   // $('#result').html('That language is profane dude.');
-   alert("profanity");
-
-}
-else{
-    //$('#result').html('That language is just fine.');
-    alert("no profanity");
 }
 
