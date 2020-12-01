c9810d6b3273f3ab3ca450530258d464f9b1b255
Brandon Ison <heroice18@gmail.com>
2020-11-30 17:00:12 -0700

    Starting to work on the server

---
+++ Node/Basic.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer(function getPred(req, res){
+
+    res.setHeader('Content-type', 'application/json');
+    res/setHeader('Access-Control-Allow-Origin', "*");
+    
+    
+    res.writeHead(200); //Status Code
+
+
+    let dataObject = {"id":1234, "name": "BOB", "email":"ASASAS"};
+    let data = JSON.stringify(dataObject);
+
+    res.end(data);
+
+});
+
+server.listen(1234, function(){
+    console.log("listening on port");
+
+})
+
+
