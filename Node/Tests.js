3760f1949c1bdedb0d8ecfaec6d00185ea255ce1
Brandon Ison <heroice18@gmail.com>
2020-12-03 09:35:57 -0700

    .

--- Node/Tests.js
+++ Node/Tests.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', "*");
  var dataToSend;
  // spawn new child process to call the python script
- const python = spawn('python', ['../Phython/test.py']);
+ const python = spawn('python', ['../Phython/text_generation.py']);
  // collect data from script
  python.stdout.on('data', function (data) {
   console.log('Pipe data from python script ...');
