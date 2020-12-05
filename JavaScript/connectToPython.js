1ffcf2d00700362a7c09c117f6877440a71c3373
Brandon Ison <heroice18@gmail.com>
2020-12-05 11:38:01 -0700

    .

--- JavaScript/connectToPython.js
+++ JavaScript/connectToPython.js
@@ -18,7 +18,7 @@ function testPhy(passingData)
         $.ajax({
         url: "http://localhost:3000",
         // dataType: "jsonp",
-        data: {}, 
+        data: '{"data": "TEST"}',
         type: 'Get',
         jsonpCallback: 'callback', // this is not relevant to the POST anymore
         success: function (data) {
