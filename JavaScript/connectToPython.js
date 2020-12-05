a495320aad09179a1bb1142ef6cbe6516fedb671
Brandon Ison <heroice18@gmail.com>
2020-12-05 11:36:51 -0700

    .

--- JavaScript/connectToPython.js
+++ JavaScript/connectToPython.js
@@ -1,11 +1,24 @@
 function testPhy(passingData)
 {
             var totalReply;
-
+            var modelType = localStorage.getItem("genre");
+            var pythonPath = '../Phython/text_generation.py';
+            if(modelType == "Fantasy")
+            {
+              pythonPath = '../Phython/Fantasy_text.py';
+            }
+            else if(modelType == "Mystery")
+            {
+              pythonPath = '../Phython/Mystery_text.py';
+            }
+            else if(modelType == "Sci Fi")
+            {
+              pythonPath = '../Phython/Sci_Fi_text.py';
+            }
         $.ajax({
         url: "http://localhost:3000",
         // dataType: "jsonp",
-        data: '{"data": "TEST"}', 
+        data: {}, 
         type: 'Get',
         jsonpCallback: 'callback', // this is not relevant to the POST anymore
         success: function (data) {
--- Node/Tests.js
+++ Node/Tests.js
@@ -4,20 +4,7 @@ const app = express()
 const port = 3000
 const python2 = spawn('python', ['../Phython/text_generation.py']);
 
-var modelType = localStorage.getItem("genre");
-var pythonPath = '../Phython/text_generation.py';
-if(modelType == "Fantasy")
-{
-  pythonPath = '../Phython/Fantasy_text.py';
-}
-else if(modelType == "Mystery")
-{
-  pythonPath = '../Phython/Mystery_text.py';
-}
-else if(modelType == "Sci Fi")
-{
-  pythonPath = '../Phython/Sci_Fi_text.py';
-}
+
 
 python2.stdout.on('data', (data) => {
 console.log("OUT: " + data);
@@ -34,7 +21,7 @@ app.get('/', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', "*");
  var dataToSend;
  // spawn new child process to call the python script
- const python = spawn('python', [pythonPath]);
+ const python = spawn('python', [python2]);
  // collect data from script
  python.stdout.on('data', function (data) {
   console.log('Pipe data from python script ...');
